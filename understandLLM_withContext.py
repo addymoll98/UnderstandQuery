a83ab20c7117665260dc82db9c40ae6b24f643a0
addymoll98@gmail.com <addymoll98@gmail.com>
2024-07-09 15:03:24 -0600

    added cl arguments for with context

--- understandLLM_withContext.py
+++ understandLLM_withContext.py
@@ -149,6 +149,12 @@ contents = """
 Model::input_vector_t interpolatedInput(const Model::input_vector_v_t &U, double t, double total_time, bool first_order_hold) { const size_t K = U.size(); const double time_step = total_time / (K - 1); const size_t i = std::min(size_t(t / time_step), K - 2); const Model::input_vector_t u0 = U.at(i); const Model::input_vector_t u1 = first_order_hold ? U.at(i + 1) : u0; const double t_intermediate = std::fmod(t, time_step) / time_step; const Model::input_vector_t u = u0 + (u1 - u0) * t_intermediate; return u; } 
 """
 
+import sys
+myPath = sys.argv[1]
+print(myPath)
+userQuestion = sys.argv[2]
+print(userQuestion)
+
 SUMMARIZE_CODE_PROMPT = """
 Generate a concise summary of the provided C++ function. 
 Use the provided context from other parts of the codebase if it helps summarize the function. 
